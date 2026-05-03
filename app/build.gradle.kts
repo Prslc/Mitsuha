aaf1962b86323d3d95badd015d81125441ad662d
Prslc <prslc113@gmail.com>
2026-05-03 11:43:43 +0800

    chore: bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         compileSdk = 37
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
 
         ndk {
             abiFilters.add("arm64-v8a")
