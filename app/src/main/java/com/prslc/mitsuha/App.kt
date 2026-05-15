365f7e080ef1ed28fef220234f2e4033284dbe78
Prslc <prslc113@gmail.com>
2026-05-16 00:40:51 +0800

    refactor: clean up redundant Xposed service binding logic
    Removed native ServiceConnection and manual binding setup, leaving initialization to the LibXposed framework itself.

--- app/src/main/java/com/prslc/mitsuha/App.kt
+++ app/src/main/java/com/prslc/mitsuha/App.kt
@@ -1,11 +1,6 @@
 package com.prslc.mitsuha
 
-import android.annotation.SuppressLint
 import android.app.Application
-import android.content.ComponentName
-import android.content.Intent
-import android.content.ServiceConnection
-import android.os.IBinder
 import android.util.Log
 import io.github.libxposed.service.XposedService
 import io.github.libxposed.service.XposedServiceHelper
@@ -32,57 +27,18 @@ class App : Application() {
         fun onServiceStateChanged(service: XposedService?)
     }
 
-    private val connection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            Log.d(TAG, "onServiceConnected: $name")
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            Log.w(TAG, "Service Disconnected")
-            mService = null
-            notifyListeners(null)
-        }
-    }
-
     override fun onCreate() {
         super.onCreate()
 
         XposedServiceHelper.registerListener(object : XposedServiceHelper.OnServiceListener {
             override fun onServiceBind(service: XposedService) {
-                Log.i(TAG, "Service Bound Successfully")
+                Log.i(TAG, "Service bind Successfully")
                 mService = service
-                notifyListeners(service)
             }
 
             override fun onServiceDied(service: XposedService) {
                 mService = null
-                notifyListeners(null)
             }
         })
-
-        bindXposedService()
-    }
-
-    @SuppressLint("QueryPermissionsNeeded")
-    private fun bindXposedService() {
-        val intent = Intent("io.github.libxposed.service.ACTION_BIND_SERVICE")
-        val resolvedInfo = packageManager.queryIntentServices(intent, 0)
-
-        if (resolvedInfo.isNotEmpty()) {
-            val serviceInfo = resolvedInfo[0].serviceInfo
-            intent.setComponent(ComponentName(serviceInfo.packageName, serviceInfo.name))
-
-            try {
-                bindService(intent, connection, BIND_AUTO_CREATE)
-            } catch (e: Exception) {
-                Log.e(TAG, "Bind failed", e)
-            }
-        } else {
-            Log.e(TAG, "Xposed Service not found")
-        }
-    }
-
-    private fun notifyListeners(service: XposedService?) {
-        listeners.forEach { it.onServiceStateChanged(service) }
     }
 }
